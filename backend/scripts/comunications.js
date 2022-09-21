ea6da51cdc3e152e2e3d0b29b51b8019e51e050e
rickyvit <ricky.vitali28@gmail.com>
2022-09-21 17:10:28 -0300

    Borrar comunicado y editarlo funcionales. Puede pulirse un poco mas

--- backend/scripts/comunications.js
+++ backend/scripts/comunications.js
@@ -3,7 +3,6 @@ async function getComunications() {
 
     try {
         const res = await fetch("http://localhost:3100/comunications")
-        console.log(res)
         const parsedReq = await res.json();
         printComunications(parsedReq)
     } catch (error) {
@@ -14,20 +13,20 @@ async function getComunications() {
 
 getComunications()
 
-
 /* PRINT DATA */
 
 const comunicationsPanel = document.getElementsByClassName("comunicationsPanel")[0]
 
 function printComunications(data) {
     console.log("Print this", data)
-/*     let comunications = data.comunications
-    let config = data.config */
 
-    console.log("Config", config)
+    comunicationsPanel.innerHTML ="";
+    
 
-    comunications.forEach(comunication => {
-        console.log(comunication)
+    data.forEach(comunication => {
+
+        let cardDiv = document.createElement("div");
+       
 
         let comunicationBody = `
         <div class="comunicationCard" id="${comunication.id}">
@@ -44,23 +43,25 @@ function printComunications(data) {
                 <input type="textarea" id="paragraph" name="paragraph" class="inputToSend" value="${comunication.paragraph}">
 
                 <div class="comunicationDate">
-                    <input type="text" id="date" name="show_new_badge_until" class="inputToSend" value="${comunication.show_new_badge_until}">
+                    <input type="text" id="show_new_badge_until" name="show_new_badge_until" class="inputToSend" value="${comunication.show_new_badge_until}">
                     <button class="preview">Previsualizar</button>
                 </div>
+                
             </div>
+            <button class="deleteButton" id="${comunication.id}">X</button>
         </div>
         `
-        comunicationsPanel.innerHTML = comunicationBody
-
+        cardDiv.innerHTML = comunicationBody;
+        comunicationsPanel.appendChild(cardDiv)
+     
     });
 
     /* Consultar con Marcos */
     for (let i = 0; i < comunicationInputs.length; i++) {
         comunicationInputs[i].addEventListener('change', waitToSend)
     }
-
+    deleteComunication()
 }
-
 
 
 /* DATA SENDER */
@@ -71,22 +72,39 @@ let sender;
 const delayToSend = 2000;
 
 function waitToSend() {
+   
+
     if (sender) clearTimeout(sender);
-    sender = setTimeout(sendToServer, delayToSend)
+    sender = setTimeout(sendToServer(this), delayToSend)
+
 }
 
 
+function sendToServer(selectedInput) {
 
+    let inputsToSend = selectedInput.parentNode.parentNode
 
+    let title = inputsToSend.querySelector('#title').value
+    let paragraph = inputsToSend.querySelector('#paragraph').value
+    let show_new_badge_until =inputsToSend.querySelector('#show_new_badge_until').value
+    let id = inputsToSend.id
 
-function sendToServer() {
-
-    console.log("Send to server")
-
-    for (let i = 0; i < comunicationInputs.length; i++) {
-        console.log(comunicationInputs[i].value)
-
+  const data = { title,paragraph, show_new_badge_until, id }
+    
+    const options = {
+        method: "PATCH",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
     }
+
+    fetch("http://localhost:3100/comunications", options)
+    
+   
+
+    console.log("I changed")
+
 
 
 }
@@ -100,11 +118,59 @@ addComunication.addEventListener('click',function(e) {
 })
 
 
+/* DELETE COMUNICATION */
+
+function deleteComunication(){
+    const deleteButton = document.getElementsByClassName('deleteButton')
+    
+    for (let i = 0; i < deleteButton.length; i++) {
+
+        deleteButton[i].addEventListener('click', function(e){
+         
+            const id = e.target.id
+            const data = { id }
+
+            const options = {
+                method: "DELETE",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+
+            fetch("http://localhost:3100/comunications",options)
+            .then(getComunications()) 
+
+
+        })
+        
+    }
+}
 
 
 
+/* EXTRAS */
 
 
 /* fetch("http://localhost:3100/comunications")
 .then(response => response.json())
 .then(data => console.log("This is my data:",data)); */
+
+
+/* const data = { username: 'example' };
+
+fetch('https://example.com/profile', {
+    method: 'POST', // or 'PUT'
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+ */
