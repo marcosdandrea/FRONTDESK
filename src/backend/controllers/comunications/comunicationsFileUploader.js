7798ef4bfde81c8aefd30461f103dfc0bf149f19
marcosdandrea <marcosdandrea@gmail.com>
2022-10-12 16:28:20 -0300

    changes in comunication endpoint

--- src/backend/controllers/comunications/comunicationsFileUploader.js
+++ src/backend/controllers/comunications/comunicationsFileUploader.js
@@ -17,15 +17,15 @@ const storage = multer.diskStorage({
     }
 })
 
-function cleanFileName( s ) {
+function cleanFileName(s) {
     return s.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
 
-  }
+}
 
 function checkFormat(req, file, cb) {
     file.originalname = Buffer.from(file.originalname, 'latin1').toString(
         'utf8',
-      );
+    );
     if (file.mimetype == "image/png" || file.mimetype == "video/mp4") {
         cb(null, true);
     } else {
@@ -34,24 +34,33 @@ function checkFormat(req, file, cb) {
     }
 }
 
-function uploadFile(req, res, next) {
-    console.log ("uploading File")
-    const upload = multer({
-        storage: storage,
-        fileFilter:
-            checkFormat,
+const upload = multer({
+    storage,
+    fileFilter:
+        checkFormat,
+}).single('media');
 
-    }).single('media');
+function uploadFile(req, res, next) {
 
     upload(req, res, function (err) {
-        if (err instanceof multer.MulterError) {
-            // A Multer error occurred when uploading.
-            res.status(403).send(err.message)
-        } else if (err) {
-            // An unknown error occurred when uploading.
-            res.status(403).send(err.message)
+        //if req.file is undefined there's no file to upload
+        let media = {}
+        if (req.file != undefined) {
+            res.multerUploadCompleted = true
+            console.log ("Media mode: File upload")
+            media = { media: { filename: req.file.filename, originalName: req.file.originalname } }
+        } else {
+            if (req.body.icon) {
+                res.multerUploadCompleted = false
+                console.log ("Media mode: Icon")
+                media = { media: { filename: req.body.icon, originalName: req.body.icon } }
+            }else{
+                res.status(400).send("You must send full information in body")
+                return
+            }
         }
-        // Everything went fine. 
+        delete req.body.icon
+        Object.assign(req.body, media)
         next()
     })
 }
